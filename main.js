5bba13ad858e50bb42cda8b7eb72ae2e0ed1bb1e
edgarp1009 <edgarp1009@gmail.com>
2024-04-09 04:01:53 +0200

    implementacion de LazyLoad, agregue animaciones en la pagina de carga y el header, implementacion de buscador de pokemon a traves de un input y un filtro de pokemon por tipo a traves de botones en el header

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const API = "https://pokeapi.co/api/v2/pokemon";
 const container = document.querySelector(".container");
 
-async function obtenerPokemon() {
+// Utilice Async/await porque los pokemon se estaban generando de manera desordenada
+async function obtenerPokemon() { 
     for (let i = 1; i <= 151; i++) {
     try{
         const response = await fetch(`${API}/${i}`);
@@ -22,10 +23,13 @@ async function obtenerPokemon() {
 // }
 
 function crearPokemon(pokemon) {
+    //Generamos los elementos <p> con los tipos correspondientes a cada pokemon
     let tipos = pokemon.types.map(tipo => `<p class="${tipo.type.name} tipo">${tipo.type.name.toUpperCase()}</p>`);
 
     tipos = tipos.join('');
 
+
+    //Generamos las cards de cada pokemon y le agregamos una clase Flipped para voltear la card
     const divCard = document.createElement("div");
     divCard.classList.add("card");
     divCard.addEventListener("click", () => divCard.classList.toggle("flipped"));
@@ -35,7 +39,7 @@ function crearPokemon(pokemon) {
     divFront.innerHTML = `
                 <p id="numeroPokemon">#${pokemon.id}</p>
                 <figure>
-                    <img class="imagen-pokemon" id="foto" src="${pokemon.sprites.other["official-artwork"]["front_default"]}" alt="${pokemon.name}">
+                    <img class="imagen-pokemon lazy" id="foto" data-src="${pokemon.sprites.other["official-artwork"]["front_default"]}" alt="${pokemon.name}">
                 </figure>
                 <div class="info-container">
                     <h1 id="nombre">${pokemon.name.toUpperCase()}</h1>
@@ -48,25 +52,78 @@ function crearPokemon(pokemon) {
     divBack.classList.add("back");
     divBack.innerHTML = `
     <h1>${pokemon.name.toUpperCase()}</h1>
-    <p class="shinny">VERSION SHINY</p><img class="imagen-back" src="${pokemon.sprites.other["official-artwork"]["front_shiny"]}" alt="">`;
+    <p class="shinny">VERSION SHINY</p><img class="imagen-back lazy" data-src="${pokemon.sprites.other["official-artwork"]["front_shiny"]}" alt="">`;
 
     container.append(divCard);
     divCard.append(divFront, divBack);
+
+    //Listeners para LazyLoad
+    window.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);
+    lazyLoad();
+}
+
+//Colocamos un event listener a cada boton de la navbar para crear un filtro de pokemon dependiendo de su clase
+
+const navbarTypes = document.querySelectorAll(".navbar-type");
+
+navbarTypes.forEach(navbarType => navbarType.addEventListener("click",() => {
+    
+    for (let i = 0; i < container.children.length; i++) {
+
+        const cardInfo = container.children[i].children[0].children[2].children;
+        const cardInfoArray = Array.from(cardInfo);
+
+        if (navbarType.classList.contains("todos")){
+            container.children[i].classList.remove("hidden");
+        } 
+        else if (!cardInfoArray.some(item => item.classList.contains(`${navbarType.classList[1]}`))){
+            container.children[i].classList.add("hidden");
+        } 
+        else {
+            container.children[i].classList.remove("hidden");
+        }
+        
+    }
+} ))
+
+//Creando un buscador de pokemon mediante un input
+
+const buscador = document.querySelector("#buscador");
+
+buscador.addEventListener("input", () => {
+    for (let i = 0; i < container.children.length; i++) {
+        const valorBuscador = buscador.value.toLowerCase();
+
+        const pokemonName = container.children[i].children[0].children[2].children[0].textContent;
+        // const cardInfoArray = Array.from(cardInfo);
+
+        if (!pokemonName.toLowerCase().includes(`${valorBuscador}`)){
+            container.children[i].classList.add("hidden");
+            console.log("no tiene");
+        } 
+        else {
+            container.children[i].classList.remove("hidden");
+            console.log("tiene");
+        }
+        
+    }
+});
+
+//Funcion de LazyLoad
+
+function lazyLoad() {
+    const lazyImages = document.querySelectorAll(".lazy");
+
+    lazyImages.forEach(image => {
+        if (image.getBoundingClientRect().top < window.innerHeight && image.getBoundingClientRect().bottom >= 0 && getComputedStyle(image).display !== "none") {
+            image.src = image.dataset.src;
+            image.classList.remove("lazy");
+
+            console.log("Lazy")
+        }
+    });
 }
 
 obtenerPokemon();
-
-
-/* <div class="pokemon card">
-                <p id="numeroPokemon">#002</p>
-                <figure>
-                    <img class="imagen-pokemon" id="foto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png" alt="IVYSAUR">
-                </figure>
-                <div class="info-container">
-                    <h1 id="nombre">IVYSAUR</h1>
-                    <p class="tipo">GRASS</p>
-                    <p class="tipo">POISON</p>
-                    <p class="altura">10m</p>
-                    <p class="peso">130Kg</p>
-                </div>
-            </div> */
